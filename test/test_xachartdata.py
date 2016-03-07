e8d963d267a3a54db9eee0ced5c67a70cd868fca
sculove <sculove@gmail.com>
2016-03-08 02:28:17 +0900

    차트 종류별 프로세스 호출 예제 추가

--- test/test_xachartdata.py
+++ test/test_xachartdata.py
@@ -19,6 +19,13 @@ if __name__ == "__main__":
             Chartdata.DAY : [ "20150101" ],
             Chartdata.WEEK : [ "20070101" ],
             Chartdata.MONTH : ("20100101", "20161231")
+        }).process({
+            "SMA" : [ 5, 10, 20, 60],   # 이동평균선
+            "BBANDS" : [20, 2], #볼랜져 밴드 period, 승수
+            "ATR" : 14, #ATR 지표 period
+            "STOCH" : [ 5, 3, 0],   #스토케스틱 K period, D period, D type
+            "MACD" : [12, 26, 9],  # short, long, signal
+            "RSI" : 14,  # period
         })
 
         # 테스트를 위한 루프
@@ -26,7 +33,7 @@ if __name__ == "__main__":
             session.heartbeat()
             chart.load({
                 5 : ["20151202"]
-            }).process({
+            }).process(5, {
                 "SMA" : [ 5, 10, 20, 60],   # 이동평균선
                 "BBANDS" : [20, 2], #볼랜져 밴드 period, 승수
                 "ATR" : 14, #ATR 지표 period
@@ -38,6 +45,7 @@ if __name__ == "__main__":
             _dayDf = chart.get(Chartdata.DAY)
             _weekDf = chart.get(Chartdata.WEEK)
             _monthDf = chart.get(Chartdata.MONTH)
+            print(_5df)
             print("5분 데이터 : %s ~ %s" % (_5df.iloc[0]["date"], _5df.iloc[len(_5df)-1]["date"]) )
             print("일 데이터 : %s ~ %s" % (_dayDf.iloc[0]["date"], _dayDf.iloc[len(_dayDf)-1]["date"]) )
             print("주 데이터 : %s ~ %s" % (_weekDf.iloc[0]["date"], _weekDf.iloc[len(_weekDf)-1]["date"]) )
